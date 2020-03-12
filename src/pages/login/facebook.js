a1747c606ba07e7d7e35d02feb1d31a82aae0ae6
kplyn <ksmonchsnok@gmail.com>
2020-03-12 11:02:31 +0700

    [🎉] deploy to firebase

--- src/pages/login/facebook.js
+++ src/pages/login/facebook.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../../style.css";
-import FacebookLoginBtn from "react-facebook-login";
+import FacebookLogin from "react-facebook-login";
 
 export default class Facebook extends Component {
   constructor(props) {
@@ -13,43 +13,54 @@ export default class Facebook extends Component {
       picture: ""
     };
   }
+  componentClicked = () => {
+    console.log("Login With Facebook");
+  };
+  responseFacebook = response => {
+    console.log(response);
+    if(response.status !== "unknown"){
+      let auth = response;
+      localStorage.setItem("FB-Login", JSON.stringify(auth));
+      this.setState({
+        auth : true,
+        name : response.name,
+        email : response.email,
+        picture : response.picture.data.url
+        
+      })
+    }
+    
+  };
 
   render() {
     let fbContent;
-    this.state.auth ? fbContent = (
-      <div></div>
-    ) : fbContent = (
-      <FacebookLogin
-    appId="186461882774241"
-    autoLoad={true}
-    fields="name,email,picture"
-    onClick={componentClicked}
-    callback={responseFacebook} />
-    );
-    return(
-      <div></div>
-    )
-    }
+    if (this.state.isLoggedIn) {
+    fbContent = null;
+    } else {
+    this.state.auth
+      ? (fbContent = <div>Hi {this.state.name}</div>)
+      : (fbContent = (
+          <FacebookLogin
+            appId="186461882774241"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={this.componentClicked}
+            callback={this.responseFacebook}
+            class="fb-login-button"
+            data-size="large"
+            data-width="300"
+            data-height="45"
+            data-button-type="login_with"
+            data-button-type="login_with"
+            data-auto-logout-link="true"
+            data-use-continue-as="false"
+            scope="public_profile,email"
+            onlogin={this.checkLoginState}
+            icon="fa-facebook"
+          />
+        
+        ));
+      }
+    return <div id="Facebook">{fbContent}</div>;
   }
-
-//     // if (this.state.isLoggedIn) {
-//     //   fbContent = null;
-//     // } else {
-//     //   fbContent = (
-//     //     <div
-//     //       scope="public_profile,email"
-//     //       onlogin={this.checkLoginState}
-//     //       class="fb-login-button"
-//     //       data-size="large"
-//     //       data-width="300"
-//     //       data-height="45"
-//     //       data-button-type="login_with"
-//     //       data-auto-logout-link="true"
-//     //       data-use-continue-as="false"
-//     //       autoLoad={true}
-//     //     />
-//     //   );
-//     // }
-//     return <div id="facebook">{fbContent}</div>;
-//   }
-// }
+}
--- src/service/firebase-config.js
+++ src/service/firebase-config.js
@@ -1,9 +1,10 @@
 export default {
-    apiKey: "AIzaSyBglgr0Nyfrh2X31JX7gmFBC78hSyTGnh0",
-    authDomain: "beautycare-9.firebaseapp.com",
-    databaseURL: "https://beautycare-9.firebaseio.com",
-    projectId: "beautycare-9",
-    storageBucket: "beautycare-9.appspot.com",
-    messagingSenderId: "509231918918",
-    appId: "1:509231918918:web:5513512e3f7636e8"
+    apiKey: "AIzaSyC9SnzFIrMF41K57it-skYg5DoQshD8uZ4",
+    authDomain: "beauty-busniess.firebaseapp.com",
+    databaseURL: "https://beauty-busniess.firebaseio.com",
+    projectId: "beauty-busniess",
+    storageBucket: "beauty-busniess.appspot.com",
+    messagingSenderId: "200711871568",
+    appId: "1:200711871568:web:cad3bdee0433a815928555",
+    measurementId: "G-9VGW3YM381"
 };
